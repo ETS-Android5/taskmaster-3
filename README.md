97ff63fbcf5a8b734155c37736eaa8998bb3440f
Shane Patrick Roach <roach.patrick.shane@gmail.com>
2022-03-22 20:39:52 -0700

    added lab 27 features

--- README.md
+++ README.md
@@ -10,22 +10,41 @@
 An android application that allows you to keep track of all your tasks.
 
 
-
 ## Change Log
-
 
 
 ### Lab 26 (3.21.22)
 
 - Added `Homepage` Page
+
 ![Homepage](/images/homepage_lab26.png)
 
 - Added `Add a Task` Page
+
 ![All Tasks](/images/allTasks_lab26.png)
 
 - Added `All Tasks` Page
+
 ![Add Tasks](/images/addTask_lab26.png)
 
+### Lab 27 (3.22.22)
 
+- Added `Settings Page`
+
+![settings](/images/settingsPage_lab27.png)
+
+- Added `Task Detail Page`
+
+![view Task 1](/images/viewTask1_lab27.png)
+
+![view Task 2](/images/viewTask2_lab27.png)
+
+- Updated `Homepage`
+
+![update Homepage](/images/homepage_lab27.png)
+
+### Testing
+
+- `SetUsernameTest` checks to see if the correct username is displayed on the homepage.
 
 ## Resources/Citations
--- app/src/main/java/com/shaneroach/taskmaster/activity/HomeActivity.java
+++ app/src/main/java/com/shaneroach/taskmaster/activity/HomeActivity.java
@@ -1,24 +1,74 @@
 package com.shaneroach.taskmaster.activity;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.shaneroach.taskmaster.R;
 
 public class HomeActivity extends AppCompatActivity {
+    public static final String TASK_TITLE_TAG = "TASK";
+    public static final String USER_USERNAME_TAG = "userUsername";
+    SharedPreferences preferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         Button buttonAddTask = findViewById(R.id.buttonAddTask);
         Button buttonAllTask = findViewById(R.id.buttonAllTask);
+        Button buttonViewTaskOne = findViewById(R.id.buttonViewTaskOneHome);
+        Button buttonViewTaskTwo = findViewById(R.id.buttonViewTaskTwoHome);
+        Button buttonViewTaskThree = findViewById(R.id.buttonViewTaskThreeHome);
+
+
+        buttonViewTaskOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //String taskName = ((EditText) findViewById(R.id.editTextViewTaskTitle)).getText().toString();
+
+                Intent goToViewTaskPageIntent = new Intent(HomeActivity.this, ViewTaskActivity.class);
+                goToViewTaskPageIntent.putExtra(TASK_TITLE_TAG, "Task 1");
+                startActivity(goToViewTaskPageIntent);
+            }
+        });
+
+        buttonViewTaskTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //String taskName = ((EditText) findViewById(R.id.editTextViewTaskTitle)).getText().toString();
+
+                Intent goToViewTaskPageIntent = new Intent(HomeActivity.this, ViewTaskActivity.class);
+                goToViewTaskPageIntent.putExtra(TASK_TITLE_TAG, "Task 2");
+                startActivity(goToViewTaskPageIntent);
+            }
+        });
+
+        buttonViewTaskThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //String taskName = ((EditText) findViewById(R.id.editTextViewTaskTitle)).getText().toString();
+
+                Intent goToViewTaskPageIntent = new Intent(HomeActivity.this, ViewTaskActivity.class);
+                goToViewTaskPageIntent.putExtra(TASK_TITLE_TAG, "Task 3");
+                startActivity(goToViewTaskPageIntent);
+            }
+        });
 
         buttonAllTask.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,7 +88,26 @@ public class HomeActivity extends AppCompatActivity {
         });
 
 
+        ImageView userSettingsImageView = (ImageView) findViewById(R.id.userSettingsImage);
 
+        userSettingsImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToUserSettingsIntent = new Intent(HomeActivity.this, UserSettingsActivity.class);
+                startActivity(goToUserSettingsIntent);
+            }
+        });
 
     }
+
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        String userUsername = preferences.getString(UserSettingsActivity.USER_USERNAME_TAG,"No nickname");
+        ((TextView) findViewById(R.id.textHomeUsernameView)).setText(getString(R.string.username_with_input, userUsername));
+
+        Log.d(TAG, "hello " + userUsername );
+    }
+
 }
